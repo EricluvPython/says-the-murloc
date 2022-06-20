641b988b5cb10def641457b6b739946131bdf7a8
DaivdYuan <47169162+DaivdYuan@users.noreply.github.com>
2022-06-20 13:11:45 +0800

    beautiful usable frontend completed and debugged  during quarantine

---
+++ frontend/src/utils.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer(props){
+    return (
+        <>
+        <div style={{position:'fixed',bottom:0,right:0,margin:'10px'}}>
+            Nerglish Translator v1.0 © 2022
+        </div>
+        </>
+    )
+}
